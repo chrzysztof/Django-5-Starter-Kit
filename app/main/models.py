b72e975589777b368a257d2713ff1ad1d070aa30
chrzysztof <neumann.krzysztof@gmail.com>
2024-05-07 23:51:39 +0200

    Add default avatar

--- app/main/models.py
+++ app/main/models.py
@@ -7,7 +7,15 @@ class Profile(models.Model):
                                 on_delete=models.CASCADE)
     date_of_birth = models.DateField(blank=True, null=True)
     photo = models.ImageField(upload_to='users/%Y/%m/%d/',
+                              null=True,
                               blank=True)
 
+    @property
+    def get_photo_url(self):
+        if self.photo and hasattr(self.photo, 'url'):
+            return self.photo.url
+        else:
+            return "https://placehold.co/180x180"
+
     def __str__(self):
-        return f'Profil użytkownika {self.user.username}'
+        return f'User profile {self.user.username}'
